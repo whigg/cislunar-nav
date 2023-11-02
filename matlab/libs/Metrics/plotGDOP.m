d117cca0d7bc9835b7e3c64aefa8fa3888a15109
mchartigan <hartigan@purdue.edu>
2023-11-02 11:18:49 -0400

    updates?

--- matlab/libs/Metrics/plotGDOP.m
+++ matlab/libs/Metrics/plotGDOP.m
@@ -13,17 +13,17 @@ t = t - t(1);                   % set t0 = 0
 % plot number of visible sats and GDOP over time
 figure();
 subplot(2,1,1);                                     % plot visible sats
-plot(t * 24 / 86400, nvis, 'LineWidth', 1.5);
-axis([0 (t(end) * 24 / 86400) 0 8]);
+plot(t / 86400, nvis, 'LineWidth', 1.5);
+axis([0 (t(end) / 86400) 0 8]);
 grid on;
 ylabel("# of Satellites in View");
 title("Satellites in View of User");
 
 subplot(2,1,2);                                     % plot GDOP
-plot(t * 24 / 86400, dop, 'LineWidth', 1.5);
-axis([0 (t(end) * 24 / 86400) 0 10]);
+plot(t / 86400, dop, 'LineWidth', 1.5);
+axis([0 (t(end) / 86400) 0 10]);
 grid on;
-xlabel("Time (hrs)"); ylabel("Geometric Dilution of Precision");
+xlabel("Time (days)"); ylabel("Geometric Dilution of Precision");
 title("Geometric Dilution of Precision for User");
 
 set(gcf, 'position', [500, 250, 750, 500]);         % fixed size
